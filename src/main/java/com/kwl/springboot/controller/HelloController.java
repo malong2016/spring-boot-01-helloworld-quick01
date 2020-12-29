4d096c0072e0d4a0ecd67208b11bc392e9f0f91c
malong2016 <1520992967@qq.com>
2020-12-29 14:47:31 +0800

    添加了/skt的映射,maven删除了spring安全

--- src/main/java/com/kwl/springboot/controller/HelloController.java
+++ src/main/java/com/kwl/springboot/controller/HelloController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//这个类的所有的方法返回的数据直接写给浏览器(如果是对象就转为JSON数据)
-/*@ResponseBody
-@Controller*/
-//替换上面二个注解,能解析跳转就跳转,不能跳转就解析为json
-//@RestController
 @Controller
 public class HelloController {
 
@@ -18,12 +13,14 @@ public class HelloController {
     public String hello(){
         return "<b>我是加粗字体</b>";
     }
-    //RESTAPI的方式
 
 
-//    @RequestMapping("/kwl")
-//    public String hello02(){
-//        return "html";
-//    }
+
+    //如果是单独的RequestMapping不能设置为void,一定要页面跳转
+    @ResponseBody
+    @RequestMapping("/skt")
+    public void hello02(){
+        System.out.println("测试skt的");
+    }
 
 }
