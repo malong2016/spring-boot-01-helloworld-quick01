1de04aad122d2c5dcd1c69c360e7f91d19fa1272
malong2016 <1520992967@qq.com>
2021-01-06 13:42:44 +0800

    我是idea008888888

--- src/test/java/com/kwl/springboot/SpringBoot01HelloworldQuickApplicationTests.java
+++ src/test/java/com/kwl/springboot/SpringBoot01HelloworldQuickApplicationTests.java
@@ -10,7 +10,7 @@ class SpringBoot01HelloworldQuickApplicationTests {
     void contextLoads() {
         System.out.println("lailehhhhhhhhhhhhhhhh回家看看");
         System.out.println("我是github007");
-        System.out.println("idea008");
+        System.out.println("idea008888888888888888");
     }
 
 }
