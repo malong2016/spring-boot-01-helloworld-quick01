617d267498cdff524495bbb6fe28fb094b80bef1
malong2016 <1520992967@qq.com>
2021-01-06 11:00:13 +0800

    xiugai01

--- src/test/java/com/kwl/springboot/SpringBoot01HelloworldQuickApplicationTests.java
+++ src/test/java/com/kwl/springboot/SpringBoot01HelloworldQuickApplicationTests.java
@@ -8,7 +8,7 @@ class SpringBoot01HelloworldQuickApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("woshikwlkwl,会不会发生冲突了?????,我在github修改了这个文件,还不会冲突吗");
+        System.out.println("lailehhhhhhhhhhhhhhhh");
         System.out.println("测试第二次提交到github");
     }
 
