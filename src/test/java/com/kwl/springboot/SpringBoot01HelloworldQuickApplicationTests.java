625345a0ea236a05772f3e6a507e7dc7a4bf0b7b
malong2016 <1520992967@qq.com>
2021-01-06 11:03:41 +0800

    xiugai03333

--- src/test/java/com/kwl/springboot/SpringBoot01HelloworldQuickApplicationTests.java
+++ src/test/java/com/kwl/springboot/SpringBoot01HelloworldQuickApplicationTests.java
@@ -8,7 +8,7 @@ class SpringBoot01HelloworldQuickApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("lailehhhhhhhhhhhhhhhh");
+        System.out.println("lailehhhhhhhhhhhhhhhh回家看看");
         System.out.println("测试第二次提交到github");
     }
 
