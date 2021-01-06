7856cd9ea1ea668406443f6f12437e79963cfbd7
malong2016 <1520992967@qq.com>
2021-01-06 11:32:34 +0800

    我是idea01

--- src/test/java/com/kwl/springboot/SpringBoot01HelloworldQuickApplicationTests.java
+++ src/test/java/com/kwl/springboot/SpringBoot01HelloworldQuickApplicationTests.java
@@ -9,7 +9,7 @@ class SpringBoot01HelloworldQuickApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("lailehhhhhhhhhhhhhhhh回家看看");
-        System.out.println("测试第二次提交到github----idea01测试 121 022222会拦截看莲花7845252445");
+        System.out.println("我是idea");
     }
 
 }
