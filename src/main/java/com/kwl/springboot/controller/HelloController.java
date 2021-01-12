417598cf128cd3fee52814cebf5442b898857e08
malong2016 <54762492+malong2016@users.noreply.github.com>
2021-01-12 19:46:59 +0800

    Update HelloController.java

--- src/main/java/com/kwl/springboot/controller/HelloController.java
+++ src/main/java/com/kwl/springboot/controller/HelloController.java
@@ -28,6 +28,7 @@ public class HelloController {
         System.out.println("有冲突2吗");
         System.out.println("有冲突2吗,idea");
         System.out.println("有冲突2红红火火恍恍惚惚吗,idea");
+        System.out.println("github");
     }
 
 }
