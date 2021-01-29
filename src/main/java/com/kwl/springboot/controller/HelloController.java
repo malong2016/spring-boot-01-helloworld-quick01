ffc8672ff965e8d802adb80561e94d3dce835fce
kuang.weilin <1520992967@qq.com>
2021-01-29 15:27:55 +0800

    和源文件一样02

--- src/main/java/com/kwl/springboot/controller/HelloController.java
+++ src/main/java/com/kwl/springboot/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
     public static void main(String[] args) {
         //测试github,同时更新文件,会不会二选一
         System.out.println("我是傻逼");
-        System.out.println("我和源文件一样");
+        System.out.println("我和源文件一样01");
     }
 }
