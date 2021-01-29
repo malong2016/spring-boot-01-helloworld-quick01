66165cc3761c5930a4e51d84bac94660eea68658
kuang.weilin <1520992967@qq.com>
2021-01-29 15:20:22 +0800

    111

--- src/main/java/com/kwl/springboot/controller/HelloController.java
+++ src/main/java/com/kwl/springboot/controller/HelloController.java
@@ -8,36 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloController {
 
-    @ResponseBody
-    @RequestMapping("/hello")
-    public String hello(){
-        return "<b>我是加粗字体</b>";
-    }
-
-
-
-    //如果是单独的RequestMapping不能设置为void,一定要页面跳转hhhrng
-    @ResponseBody
-    @RequestMapping("/skt")
-    public void hello02(){
-   //我删除了很多文件夹
-    }
-
 
     public static void main(String[] args) {
-        //测试github和idea之间的交互
-        System.out.println("idea修改区");
-        System.out.println("github修改区");
-
-
-System.out.println("我是github01,我在48行");
-
-System.out.println("github02,我在450行");
-
-
-        System.out.println("我是小红,置路由器BBBBBBBBBBBBBBBBBBB");
-        System.out.println("测试.......");
-System.out.println("测试都是对的.......");
+        //测试github,同时更新文件,会不会二选一
     }
-
 }
